40946f56a31a4b2ae841b06850ec0ea9c7ac1793
Remy Breton <breton.r49@gmail.com>
2023-03-14 19:51:33 +0100

    add dist

--- src/Layout/Main.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import "../styles/Main.scss";
-
-function Main({ children }) {
-  // function qui va venir ajouter une balise main autour des childrens
-  return <main className="blockPage">{children}</main>;
-}
-
-export default Main;
