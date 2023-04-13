5cd7635f66861499e7a25d3fcea41dbea34cb49d
Breton <breton.r49@gmail.com>
2023-04-13 22:38:40 +0200

    test

--- src/hooks/useApartments.jsx
+++ src/hooks/useApartments.jsx
@@ -6,15 +6,10 @@ const useApartments = () => {
 
   useEffect(() => {
     // hook qui va executer le composant quand il sera monté
-    const abortController = new AbortController(); // permettra l'arret du fetch
     fetch("https://remybreton.github.io/api/db.json") //, { signal: abortController.signal })
       .then((res) => res.json())
       .then((res) => setApartments(res))
       .catch(console.error);
-    return () => {
-      // function de clean up en mode dev toujours appelé en first arretera le fetch lorsce que le composant est demonté
-      abortController.abort();
-    };
   }, []);
   return apartments;
 };
